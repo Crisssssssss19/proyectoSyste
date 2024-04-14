fd300a59bfaa6e0f90790320f6f2d8346a0bef40
Cristian Peña <cristiandpm.2004@gmail.com>
2024-04-14 12:56:55 -0500

    add deploy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavBarModule } from './nav-bar/nav-bar.module';
+import { MainModule } from './main/main.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NavBarModule, MainModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
